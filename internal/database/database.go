7f7cdae691fde469cd5434099a8e4896ed37ce93
Kerrovitarr82 <miner8282@mail.ru>
2024-12-20 20:11:05 +0300

    add first creation of admin role and user

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,10 +1,15 @@
 package database
 
 import (
+	"TIPPr4/internal/models"
+	"TIPPr4/internal/myUtils"
+	"context"
+	"errors"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
 	"os"
+	"time"
 )
 
 var DB *gorm.DB
@@ -15,5 +20,46 @@ func ConnectToDB() {
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database")
+	}
+	CheckAdmin()
+}
+
+func CheckAdmin() {
+	var ctx, cancel = context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	var role models.Role
+	var user models.User
+
+	if err := DB.WithContext(ctx).First(&role, "type = ?", "ADMIN").Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			role = models.Role{
+				Type:        "ADMIN",
+				Description: "Позволяет проводить все операции",
+				CreatedAt:   time.Now(),
+				UpdatedAt:   time.Now(),
+			}
+			DB.Create(&role)
+		}
+	}
+
+	if err := DB.WithContext(ctx).First(&user, "role_id = ?", 1).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			hashedPass, err := myUtils.HashPassword(os.Getenv("FIRST_ADMIN_PASSWORD"))
+			if err != nil {
+				log.Fatal("Failed to hash password for admin")
+				return
+			}
+			user = models.User{
+				Name:        "Admin",
+				SecondName:  "1",
+				Email:       os.Getenv("FIRST_ADMIN_EMAIL"),
+				PhoneNumber: "none",
+				Password:    hashedPass,
+				RoleID:      1,
+				CreatedAt:   time.Now(),
+				UpdatedAt:   time.Now(),
+			}
+			DB.Create(&user)
+		}
 	}
 }
