0e02b6a9b06c8ebe0f4b696adf591128da56a319
Kerrovitarr82 <miner8282@mail.ru>
2024-12-20 16:37:22 +0300

    Add routes

---
+++ internal/transport/authRouter.go
@@ -0,0 +1,11 @@
+package transport
+
+import (
+	"TIPPr4/internal/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("users/signup", controllers.Signup())
+	incomingRoutes.POST("users/login", controllers.Login())
+}
