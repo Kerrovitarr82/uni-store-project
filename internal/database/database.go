60d6e049c0f992584757e640ab70880cb1ccd0f3
Kerrovitarr82 <miner8282@mail.ru>
2025-05-19 17:54:25 +0300

    Fix to fix

--- internal/database/database.go
+++ internal/database/database.go
@@ -12,14 +12,14 @@ var DB *gorm.DB
 
 func ConnectToDB() {
 	var err error
-	count := 0
 	dsn := os.Getenv("DATABASE_URL")
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil && count != 5 {
-		count++
-		log.Println("Failed to connect to database. Restart connecting...")
-		time.Sleep(5 * time.Second)
+	for count := 0; count < 5; count++ {
 		DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err == nil {
+			return // успешное подключение
+		}
+		log.Println("Failed to connect to database. Retrying...")
+		time.Sleep(5 * time.Second)
 	}
-
+	log.Fatal("Could not connect to the database after 5 attempts:", err)
 }
