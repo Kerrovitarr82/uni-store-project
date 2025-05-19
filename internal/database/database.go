b60417ebf2f731c60ea10d8150d036d34c3cdca2
Kerrovitarr82 <miner8282@mail.ru>
2025-05-19 17:48:17 +0300

    Fix connection error

--- internal/database/database.go
+++ internal/database/database.go
@@ -5,16 +5,21 @@ import (
 	"gorm.io/gorm"
 	"log"
 	"os"
+	"time"
 )
 
 var DB *gorm.DB
 
 func ConnectToDB() {
 	var err error
+	count := 0
 	dsn := os.Getenv("DATABASE_URL")
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to database")
+	if err != nil && count != 5 {
+		count++
+		log.Println("Failed to connect to database. Restart connecting...")
+		time.Sleep(5 * time.Second)
+		DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	}
 
 }
